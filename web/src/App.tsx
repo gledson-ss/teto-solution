67a9915db0412314101799870b70f358d92ef4d4
Henrique Miranda <enrque.ns@gmail.com>
2020-09-23 16:10:43 -0300

    :collision: feat(web): add theme

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import Routes from './routes';
 
 import GlobalStyles from './styles/global';
+import theme from './styles/themes';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={theme} >
       <GlobalStyles />
       <Routes />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ web/src/styles/themes.ts
@@ -0,0 +1,7 @@
+const theme = {
+  main: '#0092DD',
+  background: '#F5F5F5',
+  text: '#FFFFFF',
+};
+
+export default theme;
