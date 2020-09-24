eb516f8126afcfeeda39c83495e201726c4f9cf7
gledson-ss <glegogle84@gmail.com>
2020-09-23 21:05:15 -0300

    :hammer: chore(web): add chatbot home

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Description from '../../components/Description';
-
+import Chatbot from '../../components/Chatbot'
 const Home: React.FC = () => {
   return <Description />;
 }
