8055141261d21e64780ec46602e84d73d6356a96
Felipe Ferreira <felipeffbs3x@gmail.com>
2020-09-24 12:17:54 -0300

    feat: paypal payment

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import Description from '../../components/Description';
-import Chatbot from '../../components/Chatbot';
+import Description from "../../components/Description";
+import Chatbot from "../../components/Chatbot";
+import PaypalButton from "../../components/PaypalButton";
 
-import { Container } from './styles';
+import { Container } from "./styles";
+
+const donation = {
+  donationType: "Unique",
+  donor: {
+    name: "Felipe",
+    email: "felipeffbs3x@gmail.com",
+    number: "string",
+    birthday: "string"
+  },
+  payment: {
+    cpf: "10026736403",
+    value: 200
+  }
+};
 
 const Home: React.FC = () => {
   return (
     <Container>
       <Description />
       <Chatbot />
+      <PaypalButton donation={donation} />
     </Container>
   );
-}
+};
 
 export default Home;
