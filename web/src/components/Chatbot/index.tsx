7aebf1c0ee63b4d3146383d42142c95d2911917b
gledson-ss <glegogle84@gmail.com>
2020-09-23 21:02:23 -0300

    :hammer: chore(web): add messages

--- web/src/components/Chatbot/index.tsx
+++ web/src/components/Chatbot/index.tsx
@@ -16,43 +16,73 @@ const Chatbot: React.FC = () => {
       style={theme}
       steps={[
           {
-            id: '1',
-            message: 'Olá, seja muito bem vindo a Organização TETO :)',
-            trigger: '2',
+            id: 'welcome',
+            message: 'Olá, seja muito bem vindo a Organização TETO',
+            trigger: 'help',
           },
           {
-            id: '2',
-            message: 'Através da mobilização de moradores e jovens voluntários, construímos moradias de emergência e soluções de infraestrutura',
-            trigger: '3'
+            id: 'help',
+            message: 'Ajude-nos com alguma doação :)',
+            trigger: 'options'
           },
           {
-            id: '3',
+            id: 'options',
             options:[
               {
-                value: 1, label: 'Saber mais sobre a TETO.', trigger: '4'
+                value: 1, label: 'Saber mais sobre a TETO', trigger: 'description_options'
               },
               {
-                value: 2, label: 'Quero fazer uma doação!', trigger: '5'
-              },
-            ]
-          },
-          {
-            id: '4',
-            options: [
-              {
-                value: 1, label: 'SEJA UM AMIGO DA TETO', trigger: '1',
+                value: 2, label: 'Quero fazer uma doação!', trigger: 'donation'
               },
               {
-                value: 2, label: 'POR QUE DOAR?', trigger: '1'
-              },
-              {
-                value: 3, label: 'SOBRE A TETO', trigger: '1'
+                value: 3, label: 'Quero encerrar a conversa, obrigado', trigger: 'end'
               }
             ]
           },
           {
-            id: '5',
-            message: 'fim',
+            id: 'description_options',
+            options: [
+              {
+                value: 1, label: 'SEJA UM AMIGO DA TETO', trigger: 'description_options_1',
+              },
+              {
+                value: 2, label: 'POR QUE DOAR?', trigger: 'description_options_2'
+              },
+              {
+                value: 3, label: 'SOBRE A TETO', trigger: 'description_options_3'
+              }
+            ]
+          },
+          {
+            id: 'description_options_1',
+            message: 'Nós trabalhamos pela superação da pobreza nas favelas mais precárias do país.',
+            trigger: 'description_link'
+          },
+          {
+            id: 'description_options_2',
+            message: 'Doando um pequeno valor a cada mês, você ajuda a atuação da TETO em 6 estados brasileiros.',
+            trigger: 'description_link'
+          },
+          {
+            id: 'description_options_3',
+            message: 'Através da mobilização de moradores e jovens voluntários, construímos moradias de emergência e soluções de infraestrutura.',
+            trigger: 'description_link'
+          },
+          {
+            id: 'description_link',
+            options:[
+              {value:1, label: 'Acesse o link para mais informações: [link]', trigger:'options'},
+              {value:2, label: 'Quero fazer uma doação agora!', trigger: 'donation'},
+              {value:3, label: 'Quero encerrar a conversa, obrigado', trigger: 'end'}
+            ]
+          },
+          {
+            id: 'donation',
+            message: 'blz fera'
+          },
+          {
+            id: 'end',
+            message: 'agradeçemos sua visita, volte sempre!!'
           }
         ]}
       />
