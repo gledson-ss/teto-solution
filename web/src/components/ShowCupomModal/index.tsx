10c33c4c5665a321a219b495bc8d4ecc760d2776
Henrique Miranda <enrque.ns@gmail.com>
2020-09-25 08:26:04 -0300

    :collision: feat: add discount and cupom props on show cupom modal component

--- web/src/components/ShowCupomModal/index.tsx
+++ web/src/components/ShowCupomModal/index.tsx
@@ -8,6 +8,8 @@ Container.setAppElement("#root");
 
 interface ModalProps extends ReactModalProps {
   onClose: () => void;
+  cupom: string;
+  discount: string;
 }
 
 const ModalPortal: React.FC = ({ children }) => {
@@ -17,16 +19,23 @@ const ModalPortal: React.FC = ({ children }) => {
   );
 };
 
-const ShowCupomModal: React.FC<ModalProps> = ({ onClose, ...rest }) => {
+const ShowCupomModal: React.FC<ModalProps> = ({
+  cupom,
+  discount,
+  onClose,
+  ...rest
+}) => {
   return (
     <ModalPortal>
       <Container {...rest}>
         <h1>Obrigado pela sua doação!!</h1>
         <p>
-          Estamos tão felizes com isso, que decidimos da-lo um pequeno presente,
-          um cupom de desconto de 10% na loja da Dream Team. Espero que goste!
-          :)
+          {`Estamos tão felizes com isso, que decidimos da-lo um pequeno presente,
+          um cupom de desconto de ${discount} na loja da Dream Team. Espero que goste!
+          :)`}
         </p>
+        <p>{`Seu cupom: ${cupom}`}</p>
+
         <button type="button" onClick={onClose}>
           Fechar
         </button>
